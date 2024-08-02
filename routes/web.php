a0a1611280b3d51d8eb0cb9d77c4152691d9a2c9
humphreyokoth <humpasp@gmail.com>
2024-08-02 12:34:25 +0300

    feat:models

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,22 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function ()  {
     return view('jobs', [
-        'jobs' => [
-            [    'id'=> 1 ,
-                'title' => 'Director',
-                'salary' => "$50,000"
-            ],
-            [    'id'=> 2,
-                'title' => 'Programmer',
-                'salary' => '$70,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
-Route::get('/jobs/{id}',function ($id){
-       $jobs = [
-            [   'id'=> 1 ,
-                'title' => 'Director',
-                'salary' => "$50,000"
-            ],
-            [
-                'id'=>2,
-                'title' => 'Programmer',
-                'salary' => '$70,000'
-            ]
-            ];
-    $job = Arr::first($jobs,fn($job)=>$job['id']==$id);
-    return view('job',['job'=>$job]);
-
+Route::get('/jobs/{id}', function ($id)  {
+$job = Job::find($id);
+    return view('job', ['job' => $job]);
 });
 Route::get('/contact', function () {
     return view('contact');
