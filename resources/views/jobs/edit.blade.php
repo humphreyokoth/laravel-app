1685a68701e439a4564c4d89f1938bbdb029f17e
humphreyokoth <humpasp@gmail.com>
2024-08-03 08:14:22 +0300

    feat:delete

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -5,7 +5,7 @@
 
     <form method="POST" action="/jobs/{{$job->id}}">
         @csrf
-        $@method('PATCH')
+        @method('PATCH')
 
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
@@ -66,11 +66,22 @@
 
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <a  href="/jobs/{{ $job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+        <div class="mt-6 flex items-center justify-between gap-x-6">
+
+            <div class="flex items-center">
+                <button form="delete-form" class=" text-red-500 text-sm font-bold">Delete</button>
+            </div>
+            <div class="flex items-center gap-x-6">
+                <a  href="/jobs/{{ $job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                <button type="submit"
+                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>  
+            </div>
+            
         </div>
+    </form>
+    <form method="POST" action="/jobs/{{$job->id}}" id="delete-form" class="hidden">
+        @csrf
+        @method('DELETE')
     </form>
 
 </x-layout>
