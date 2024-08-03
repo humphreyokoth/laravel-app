630735f6db98af6103d34093b7be2c02db9bc18a
humphreyokoth <humpasp@gmail.com>
2024-08-03 08:02:47 +0300

    feat:patch

---
+++ resources/views/jobs/edit.blade.php
@@ -0,0 +1,76 @@
+<x-layout>
+    <x-slot:heading>
+        Edit job {{ $job->title }}
+    </x-slot:heading>
+
+    <form method="POST" action="/jobs/{{$job->id}}">
+        @csrf
+        $@method('PATCH')
+
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+                <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful
+                    what you share.</p>
+
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+
+                                <input type="text" name="title" id="title" autocomplete="title"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="Shift leader" 
+                                    value="{{$job->title}}">
+                            </div>
+                            @error('title')
+                                <p class="text-xs text-red-500"> {{ $message }}</p>
+                            @enderror
+                        </div>
+
+                    </div>
+                    <div class="sm:col-span-4">
+                        <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+
+                                <input type="text" name="salary" id="salary" autocomplete="salary"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="$50,000" 
+                                    
+                                    value="{{$job->salary}}">
+                            </div>
+                            @error('salary')
+                                <p class="text-xs text-red-500"> {{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+
+                </div>
+                {{-- <div>
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-500">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+
+
+                </div> --}}
+            </div>
+
+
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <a  href="/jobs/{{ $job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <button type="submit"
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+        </div>
+    </form>
+
+</x-layout>
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,10 +1,23 @@
-<x-layout>
+{{-- <x-layout>
     <x-slot:heading>
        Job 
     </x-slot:heading>
     <h1>Hello About page</h1>
     {{-- @foreach ($jobs as $job) --}}
-    <li>{{$job['title']}} Pays {{$job['salary']}}</li>
+    {{-- <li>{{$job['title']}} Pays {{$job['salary']}}</li> --}}
 {{--     
 @endforeach --}}
+{{-- </x-layout> --}} 
+
+<x-layout>
+    <x-slot:heading>
+       Job 
+    </x-slot:heading>
+    <h2>{{$job->title}}</h2>
+
+    <p>This job pays {{$job->salary}}</p>
+    <p class="mt-6">
+        <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
+
+    </p>
 </x-layout>
